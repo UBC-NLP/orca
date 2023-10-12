69980b7fdc8cad98d8ddfbce472e813430b5766e
AbdelRahim A. Elmadany <elmadany@gmail.com>
2023-10-12 16:40:01 -0700

    Update run_orca_tokens_trainer.py

--- fine-tuning_code/run_orca_tokens_trainer.py
+++ fine-tuning_code/run_orca_tokens_trainer.py
@@ -292,7 +292,8 @@ def main():
         for label in labels:
             unique_labels = unique_labels | set(label)
         label_list = list(unique_labels)
-        # label_list.append("O") # for test set
+        if "O" not in label_list:
+          label_list.append("O") # for test set
         label_list.sort()
         # print(">>>>>>>label_list=", label_list)
         return label_list
